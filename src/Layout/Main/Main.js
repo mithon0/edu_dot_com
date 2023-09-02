90f8fe22ca165108b42ee372888b31f080e25b51
mithon0 <mithonhossen7244@gmail.com>
2023-09-02 12:23:32 +0600

    Footer

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navber from '../../Shared/Navber/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
           <Navber></Navber>  
           <Outlet></Outlet>
+          <Footer></Footer>
         </div>
     );
 };
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FaEnvelope, FaFacebook, FaLinkedin, FaMapMarkerAlt,FaPhoneAlt, FaTwitter, FaYoutube} from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='w-full bg-indigo-900 text-indigo-200 grid grid-cols-4 p-20'>
+            <div className='mx-auto'>
+                <h1 className='text-2xl text-orange-500 ms-10 mb-2'>edu.<span className='text-blue-500'>Com</span></h1>
+                <div>
+                    <div className='flex gap-5 items-center'>
+                        <FaMapMarkerAlt className='text-orange-400 '/>
+                        <h4 className='text-base'>Jamalgonj ,Joypurhat /Rajshahi</h4>
+                    </div>
+                    <div className='flex gap-5 items-center'>
+                        <FaPhoneAlt className='text-orange-400 '/>
+                        <h4 className='text-base'>(+880)01759555317</h4>
+                    </div>
+                    <div className='flex gap-5 items-center'>
+                        <FaEnvelope className='text-orange-400 '/>
+                        <h4 className='text-base'>mithonhossen7244@gmail.com</h4>
+                    </div>
+                    
+                </div>
+                <div className='flex gap-10 mt-4 mx-auto'>
+                    <FaFacebook/>
+                    <FaLinkedin/>
+                    <FaTwitter/>
+                    <FaYoutube/>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     );
 };
